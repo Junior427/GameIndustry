825ed666df52f1e86daf84736cca28d4df7eb42c
Lupook <lupook1998@mail.ru>
2024-05-22 21:21:21 +0300

    Dependency injection: singleton transient

--- GameIndustry/Program.cs
+++ GameIndustry/Program.cs
@@ -7,5 +7,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddSingleton<SingletonService>();
+builder.Services.AddTransient<TransientService>();
 await builder.Build().RunAsync(); 
