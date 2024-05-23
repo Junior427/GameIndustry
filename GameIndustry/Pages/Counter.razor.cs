9be90cbce6b7a182f68ab48b5d3bdcc21f3f6381
Lupook <lupook1998@mail.ru>
2024-05-23 22:07:46 +0300

    partional class

---
+++ GameIndustry/Pages/Counter.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace GameIndustry.Pages
+{
+    public partial class Counter
+    {
+        [Inject]
+        public SingletonService singleton { get; set; }
+        [Inject]
+        public TransientService transirnt { get; set; }
+
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+            singleton.Value = currentCount;
+            transirnt.Value = currentCount;
+        }
+    }
+}
